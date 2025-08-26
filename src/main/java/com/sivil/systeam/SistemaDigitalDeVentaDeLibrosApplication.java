7a94fef40533462938415b6df49bc312138c4436
kenetcode <k2ken2424@gmail.com>
2025-08-26 00:27:43 -0600

    Estructura inicial del proyecto

---
+++ src/main/java/com/sivil/systeam/SistemaDigitalDeVentaDeLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.sivil.systeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDigitalDeVentaDeLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDigitalDeVentaDeLibrosApplication.class, args);
+	}
+
+}
