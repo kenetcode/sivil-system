01a855b226f39103173cf7d2a2f73077c31bd0ae
kenetcode <k2ken2424@gmail.com>
2025-08-31 22:36:13 -0600

    Modificacion de librerias en securityconfig y cambio de validate a update en spring.jpa.hibernate.ddl-auto=update

--- src/main/java/com/sivil/systeam/config/SecurityConfig.java
+++ src/main/java/com/sivil/systeam/config/SecurityConfig.java
@@ -1,14 +1,14 @@
 package com.sivil.systeam.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class SecurityConfig {
 
     /*@Bean
